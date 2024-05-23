5c6a38f26fd2ef2f06832308dee541ed415d3913
ImMamad79 <MohammadShams956@gmail.com>
2024-05-23 10:05:58 +0330

    Implemented Template

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views import View
 
-# Create your views here.
+class Index(View):
+    def get(self, request):
+        context = {
+            
+        }
+        return render(request, 'Main/index.html', context=context)
